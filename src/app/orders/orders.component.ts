8cdf6d9d39e752b539e8e6c859199e753693ab9e
mattfu10 <mattjlocher@gmail.com>
2018-10-18 17:31:40 -0700

    add existing file

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -3,6 +3,15 @@ import { Router } from '@angular/router';
 import { FlexModalService } from '../shared-components/flex-modal/flex-modal.service';
 import { Http } from '@angular/http';
 
+export interface Iorder {
+  pid?: string;
+  image?: string;
+  description?: string;
+  price?: number;
+  quantity?: number;
+
+
+}
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
@@ -11,17 +20,61 @@ import { Http } from '@angular/http';
 
 export class OrdersComponent implements OnInit {
 
-  orders: Array<any> = [];
+  orders: Array<Iorder> = [];
 
   constructor(
+
     private router: Router,
     private flexModal: FlexModalService,
     private http: Http
-  ) {
+  ) {    this.orders = [{
+    "pid": "1",
+    "image": "assets/sm_android.jpeg",
+    "description": "Android",
+    "price": 150.00,
+    "quantity": 2
+  }, {
+    "pid": "2",
+    "image": "assets/sm_iphone.jpeg",
+    "description": "IPhone",
+    "price": 200.00,
+    "quantity": 1
+  }, {
+    "pid": "3",
+    "image": "assets/sm_windows.jpeg",
+    "description": "Windows Phone",
+    "price": 110.00,
+    "quantity": 2
+  }];
+
+
   }
 
   async ngOnInit() {
 
   }
+displayOrders()
+{this.orders = [{
+  "pid": "1",
+  "image": "assets/sm_android.jpeg",
+  "description": "Android",
+  "price": 150.00,
+  "quantity": 2
+}, {
+  "pid": "2",
+  "image": "assets/sm_iphone.jpeg",
+  "description": "IPhone",
+  "price": 200.00,
+  "quantity": 1
+}, {
+  "pid": "3",
+  "image": "assets/sm_windows.jpeg",
+  "description": "Windows Phone",
+  "price": 110.00,
+  "quantity": 2
+}];
 
-}
+}}
+  
+
+  
